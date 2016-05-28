5fb7555b4b4532580dec7f333284d64daf1cec26
mesmerizinghermit <m@jellygo.com>
2016-05-29 01:46:21 +0800

    完成布局, 等待数据测试

--- app/src/main/java/xdu/hwding/aeolosxdu/NewAppWidget.java
+++ app/src/main/java/xdu/hwding/aeolosxdu/NewAppWidget.java
@@ -4,7 +4,11 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.StrictMode;
 import android.widget.RemoteViews;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import FooPackage.ECard;
 
@@ -16,12 +20,17 @@ public class NewAppWidget extends AppWidgetProvider {
 
     static ECard eCard;
     static SharedPreferences sharedPreferences;
+    static boolean isVerified = false;
+    static ArrayList<String> eCard_strs;
 
     static void setProperties(ECard eCard,
                               Context context) {
 
         NewAppWidget.eCard = eCard;
         sharedPreferences = context.getSharedPreferences("ACCOUNT_INFO", 0);
+        isVerified = true;
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
     static void updateAppWidget(Context context,
@@ -31,6 +40,15 @@ public class NewAppWidget extends AppWidgetProvider {
         //Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
         // Instruct the widget manager to update the widget
+        if (isVerified) {
+            try {
+                eCard_strs = eCard.queryTransferInfo("2016-04-21", "2016-05-21");
+                views.setTextViewText(R.id.balance, "￥ "+eCard_strs.get(eCard_strs.size()-2));
+                views.setTextViewText(R.id.latest_consumption, "￥ "+eCard_strs.get(eCard_strs.size()-3));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
